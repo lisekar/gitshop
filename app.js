ec8020e8714dcd761ba082958f9560e4f282ba08
Livi Sekar <livisekar@livi-mac.local>
2025-11-27 22:49:41 +0530

    Gitshop initial commit :  basic express setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to GitShop!")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running in port : ${PORT}`);
+})
